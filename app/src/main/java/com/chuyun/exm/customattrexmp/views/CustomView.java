d974f1c47d515a61ef32109e6bbf87e8d4d659e7
xue <384324069@qq.com>
2017-11-27 16:05:24 +0800

    modify code

--- app/src/main/java/com/chuyun/exm/customattrexmp/views/CustomView.java
+++ app/src/main/java/com/chuyun/exm/customattrexmp/views/CustomView.java
@@ -27,6 +27,7 @@ public class CustomView extends LinearLayout{
 
     public CustomView(Context context, AttributeSet attrs) {
         this(context, attrs, R.attr.custom_style);
+
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -38,26 +39,36 @@ public class CustomView extends LinearLayout{
         color2 = a.getColor(R.styleable.custom_attrs_custom_color2,0xffff0000);
         color3 = a.getColor(R.styleable.custom_attrs_custom_color3,0xffff0000);
         color4 = a.getColor(R.styleable.custom_attrs_custom_color4,0xffff0000);
-        color5 = a.getColor(R.styleable.custom_attrs_custom_color4,0xffff0000);
+        color5 = a.getColor(R.styleable.custom_attrs_custom_color5,0xffff0000);
 
 
         TextView textView1 = new TextView(context);
         textView1.setText("color1"+Integer.toHexString(color1));
+        textView1.setTextColor(color1);
         addView(textView1);
 
         TextView textView2 = new TextView(context);
         textView2.setText("color2"+Integer.toHexString(color2));
+        textView2.setTextColor(color2);
+
         addView(textView2);
+
         TextView textView3 = new TextView(context);
         textView3.setText("color3"+Integer.toHexString(color3));
+        textView3.setTextColor(color3);
+
         addView(textView3);
 
         TextView textView4 = new TextView(context);
         textView4.setText("color4"+Integer.toHexString(color4));
+        textView4.setTextColor(color4);
+
         addView(textView4);
 
         TextView textView5 = new TextView(context);
         textView5.setText("color5"+Integer.toHexString(color5));
+        textView5.setTextColor(color5);
+
         addView(textView5);
 
         a.recycle();
